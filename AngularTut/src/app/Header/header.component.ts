84ee297902c2ebe8dcc566b33cefefa731fb9f1d
PrathameshSystenics <prathamesh.d@systenics.com>
2024-10-22 12:20:20 +0530

    Added the Bootstrap for Styling

--- AngularTut/src/app/Header/header.component.ts
+++ AngularTut/src/app/Header/header.component.ts
@@ -4,7 +4,7 @@ import { Component } from '@angular/core';
   selector: 'app-header',
   templateUrl: './header.component.html',// Specifying the template or view
   styleUrl: './header.component.css', // specifying the css url to apply to only the html template or view.
-  styles: [`li{font-size:23px;}`] ,// list of css 
+  styles: [] ,// list of css
 })
 export class HeaderComponent {
 
