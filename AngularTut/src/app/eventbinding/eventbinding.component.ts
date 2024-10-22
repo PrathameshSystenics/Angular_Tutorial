6b6b6c888ea4b4c9d2cb7740d71706eee186afd2
PrathameshSystenics <prathamesh.d@systenics.com>
2024-10-22 18:04:07 +0530

    Event Binding

--- AngularTut/src/app/eventbinding/eventbinding.component.ts
+++ AngularTut/src/app/eventbinding/eventbinding.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-eventbinding',
+  selector: 'eventbinding',
   templateUrl: './eventbinding.component.html',
   styleUrl: './eventbinding.component.css'
 })
 export class EventbindingComponent {
+  username: string = "Prathamesh"
 
+  // Event binded function
+  onUsernameChange(event: any) {
+    console.log()
+    this.username = event.target.value;
+  }
 }
