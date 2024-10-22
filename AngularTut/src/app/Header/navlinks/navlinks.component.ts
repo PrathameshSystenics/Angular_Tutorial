e791a1b104dd083575e92e053391770b6a07eec0
PrathameshSystenics <prathamesh.d@systenics.com>
2024-10-22 12:34:40 +0530

    Nested Components Creation

---
+++ AngularTut/src/app/Header/navlinks/navlinks.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-navlinks',
+  templateUrl: './navlinks.component.html',
+  styleUrl: './navlinks.component.css'
+})
+export class NavlinksComponent {
+
+}
