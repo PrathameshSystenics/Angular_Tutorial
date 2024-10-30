09581bda88c5ce0ae4f67cea6dec116291112bab
Prathameshdhande22 <prathameshdhande534@gmail.com>
2024-10-30 23:15:42 +0530

    RouterLink directive Tutorial

--- AngularTut/src/app/Header/navlinks/navlinks.component.ts
+++ AngularTut/src/app/Header/navlinks/navlinks.component.ts
@@ -3,7 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-navlinks',
   templateUrl: './navlinks.component.html',
-  styleUrl: './navlinks.component.css'
 })
 export class NavlinksComponent {
 
