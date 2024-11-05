8bfc62bf00951bdebc846b8891b21deb93508509
PrathameshSystenics <prathamesh.d@systenics.com>
2024-11-05 12:46:26 +0530

    SetValue and PatchValue method and Reset Form

--- AngularTut/src/app/templatedriven/templatedriven.component.ts
+++ AngularTut/src/app/templatedriven/templatedriven.component.ts
@@ -14,4 +14,34 @@ export class TemplatedrivenComponent {
   onSubmit() {
     console.log(this.form);
   }
+
+  onSetBtn() {
+    // for setting the value the structure must be same as the form
+    this.form.form.setValue({
+      firstname: 'firstname',
+      lastname: 'lastname',
+      email: 'email@gmail.com',
+      gender: 'Male',
+      dob: '2003-04-22',
+      address: {
+        state: 'Gujarat',
+        city: 'ahemdabad',
+      },
+    });
+  }
+
+  onPatchBtn() {
+    // setting only the specified key without interferring the others fields of the forms
+    this.form.form.patchValue({
+      address: {
+        city: 'Vangaon',
+        state: 'Maharashtra',
+      },
+    });
+  }
+
+  resetForm() {
+    // reseting the form with the reset function present in the ngForm class.
+    this.form.resetForm();
+  }
 }
